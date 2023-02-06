3e0a29ce0e88ab950dcd824048e478f65afcd41c
jorgegoco <jorgegoco70@gmail.com>
2023-02-06 12:28:13 +0100

    Setup linters

---
+++ person.rb
@@ -0,0 +1,19 @@
+# class Person
+#   def initialize(name = "Unknown", age, parent_permission = true)
+#     @name = name
+#     @age = age
+#     @parent_permission = parent_permission
+#   end
+
+#   attr_accessor :name
+#   attr_accessor : age
+#   attr_reader :id
+
+#   def is_of_age?
+
+#   end
+
+#   def can_use_services?
+#   end
+
+# end
