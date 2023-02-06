8edf6a1bb2a9d76f599c9b86461dada68dffbccc
jorgegoco <jorgegoco70@gmail.com>
2023-02-06 13:51:01 +0100

    Add Person class

--- person.rb
+++ person.rb
@@ -1,19 +1,22 @@
-# class Person
-#   def initialize(name = "Unknown", age, parent_permission = true)
-#     @name = name
-#     @age = age
-#     @parent_permission = parent_permission
-#   end
+class Person
+  def initialize(name = "Unknown", age, parent_permission = true)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
 
-#   attr_accessor :name
-#   attr_accessor : age
-#   attr_reader :id
+  attr_accessor :name
+  attr_accessor : age
+  attr_reader :id
 
-#   def is_of_age?
+  def is_of_age?
+    if @age >= 18
+  end
 
-#   end
+  private : is_of_age?
 
-#   def can_use_services?
-#   end
+  def can_use_services?
+    if is_of_age || parent_permission
+  end
 
-# end
+end
