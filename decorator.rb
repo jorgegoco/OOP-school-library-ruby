2f51e8365357c3f4f8569b2deba4f43417a6719d
jorgegoco <jorgegoco70@gmail.com>
2023-02-07 15:18:15 +0100

    Implement the base Decorator

---
+++ decorator.rb
@@ -0,0 +1,11 @@
+require_relative('./nameable')
+
+class Decorator < Nameable
+  def initialize(nameable = Nameable.new())
+    @nameable = nameable
+  end
+
+  def correct_name
+    @nameable.correct_name
+  end
+end
