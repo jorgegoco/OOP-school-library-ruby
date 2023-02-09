1cf4a464abfa5faa1f9e62dfc047063d362937a2
jorgegoco <jorgegoco70@gmail.com>
2023-02-09 19:36:58 +0100

    Add functionality to 'create a rental'

--- app.rb
+++ app.rb
@@ -1,11 +1,13 @@
 require_relative './student'
 require_relative './teacher'
 require_relative './book'
+require_relative './rental'
 
 class App
   def initialize
     @people = []
     @books = []
+    @rentals = []
   end
 
   def list_all_books
@@ -48,12 +50,12 @@ class App
       end
 
     when 2
-      puts 'Specialization:'
-      specialization = gets.chomp.to_s
       puts 'Age:'
       age = gets.chomp.to_i
       puts 'Name:'
       name = gets.chomp.to_s
+      puts 'Specialization:'
+      specialization = gets.chomp.to_s
       @people.push(Teacher.new(specialization, age, name))
       puts 'Person created successfully'
     else
@@ -68,5 +70,31 @@ class App
     author = gets.chomp.to_s
     @books.push(Book.new(title, author))
     puts 'Book created successfully'
+  end
+
+  def create_rental
+    if @books.empty?
+      puts 'Empty list of books!'
+    elsif @people.empty?
+      puts 'Empty list of people!'
+    else
+      puts 'Select a book from the following list by number'
+      @books.map.with_index { |book, index| puts "#{index}) Title: \"#{book.title}\", Author: #{book.author}" }
+      book_idx = gets.chomp.to_i
+      return puts 'Invalid Input' if(book_idx >= @books.length || book_idx < 0)
+
+      puts 'Select a person from the following lists by number (not id)' 
+      @people.map.with_index do |person, index|
+        puts "#{index}) [#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+      end
+      person_idx = gets.chomp.to_i
+      return puts 'Invalid Input' if (person_idx >= @people.length || person_idx < 0)
+    
+      print 'Date: '
+      date = gets.chomp.to_s
+
+      @rentals.push(Rental.new(date, @people[person_idx], @books[book_idx]))
+      puts 'Rental created successfully'   
+    end
   end
 end
