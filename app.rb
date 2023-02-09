14a97cf33f0017b25b043d1bc2cb2536e1bbb493
jorgegoco <jorgegoco70@gmail.com>
2023-02-09 17:25:18 +0100

    Add entry point to the app

--- app.rb
+++ app.rb
@@ -1,17 +1,2 @@
 class App
-  def initialize
-    @user_menu = "
-Please choose an option by entering a number:
-1 - List all books
-2 - List all people
-3 - Create a person
-4 - Create a book
-5 - Create a rental
-6 - List all rentals for a given person id
-7 - Exit"
-  end
-
-  def start
-    puts @user_menu
-  end
 end
