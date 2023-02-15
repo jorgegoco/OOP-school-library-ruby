4902160bf417efd4954ee5201ce807ac2996b544
jorgegoco <jorgegoco70@gmail.com>
2023-02-15 12:13:38 +0100

    Add unit tests for Student and Teacher classes

--- person.rb
+++ person.rb
@@ -15,7 +15,7 @@ class Person < Nameable
   end
 
   attr_accessor :name, :age, :rentals, :parent_permission
-  attr_reader :id
+  attr_reader :id, :classroom
 
   def correct_name
     @name
---
+++ spec/student_spec.rb
@@ -0,0 +1,20 @@
+require_relative '../student'
+
+describe Student do
+  before(:each) do
+    @student = Student.new(nil, 17, 'test_name', parent_permission: false)
+  end
+  context 'Unit testing in the Student class' do
+    it 'test creating an instance' do
+      expect(@student.classroom).to be_nil
+      expect(@student.age).to be < 18
+      expect(@student.name).to eq 'test_name'
+      expect(@student.parent_permission).to be false
+      expect(@student.id).to be_between(1, 1000)
+    end
+
+    it 'test play_hooky method' do
+      expect(@student.play_hooky).to eq '¯\(ツ)/¯'
+    end
+  end
+end
