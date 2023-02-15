e459e4ee924b401e0341b317d6d6703692b97d19
Maaloum <50062179+maaloum@users.noreply.github.com>
2023-02-15 17:26:53 +0300

    fix linters

--- spec/decotaror_spec.rb
+++ spec/decotaror_spec.rb
@@ -4,15 +4,15 @@ require_relative '../nameable'
 
 describe Decorator do
   before(:each) do
-      @nam = Nameable.new
-      @dec = Decorator.new(@nam)
+    @nam = Nameable.new
+    @dec = Decorator.new(@nam)
   end
   it 'tests decorator creation' do
     expect(@dec).to be_an_instance_of(Decorator)
   end
-  it "tests decorator method" do
-    expect { @dec.correct_name }.to raise_error(NotImplementedError, "Nameable has not implemented method 'correct_name'")
+  it 'tests decorator method' do
+    expect do
+      @dec.correct_name
+    end.to raise_error(NotImplementedError, "Nameable has not implemented method 'correct_name'")
   end
-
-
 end
--- spec/nameable_spec.rb
+++ spec/nameable_spec.rb
@@ -4,9 +4,12 @@ describe Nameable do
   before(:each) do
     @nam = Nameable.new
   end
-  it "tests creation of an instance from Neamable" do
+  it 'tests creation of an instance from Neamable' do
     expect(@nam).to be_an_instance_of(Nameable)
   end
-  it "tests Neamble method" do
-    expect { subject.correct_name }.to raise_error(NotImplementedError, "Nameable has not implemented method 'correct_name'")  end
+  it 'tests Neamble method' do
+    expect do
+      subject.correct_name
+    end.to raise_error(NotImplementedError, "Nameable has not implemented method 'correct_name'")
+  end
 end
